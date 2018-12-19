7c908ca2d325855e0d2981cf92ea0665b48a819b
Marcio Dos Santos <mvs@amazon.com>
2018-12-18 16:47:51 -0800

    Fix flaky test

--- test/unit/test_mpi.py
+++ test/unit/test_mpi.py
@@ -13,8 +13,9 @@
 from __future__ import absolute_import
 
 import inspect
+import os
 
-from mock import MagicMock, patch, ANY
+from mock import ANY, MagicMock, patch
 
 import libchangehostname
 from sagemaker_containers import _env, _mpi
@@ -83,43 +84,45 @@ def test_mpi_worker_run_no_wait(popen, ssh_client):
 @patch('subprocess.Popen')
 @patch('sagemaker_containers.training_env')
 def test_mpi_master_run(training_env, popen, policy, ssh_client):
-    master = _mpi.MasterRunner(user_entry_point='train.sh',
-                               args=['-v', '--lr', '35'],
-                               env_vars={'LD_CONFIG_PATH': '/etc/ld'},
-                               master_hostname='algo-1',
-                               hosts=['algo-1', 'algo-2'],
-                               process_per_host=2,
-                               custom_mpi_options='-v --lr 35',
-                               network_interface_name='ethw3')
+    with patch.dict(os.environ, clear=True):
 
-    process = master.run(wait=False)
+        master = _mpi.MasterRunner(user_entry_point='train.sh',
+                                   args=['-v', '--lr', '35'],
+                                   env_vars={'LD_CONFIG_PATH': '/etc/ld'},
+                                   master_hostname='algo-1',
+                                   hosts=['algo-1', 'algo-2'],
+                                   process_per_host=2,
+                                   custom_mpi_options='-v --lr 35',
+                                   network_interface_name='ethw3')
 
-    ssh_client().load_system_host_keys.assert_called()
-    ssh_client().set_missing_host_key_policy.assert_called_with(policy())
-    ssh_client().connect.assert_called_with('algo-2', port=22)
-    ssh_client().close.assert_called()
+        process = master.run(wait=False)
 
-    popen.assert_called_with([
-        'mpirun',
-        '--host', 'algo-1:2,algo-2:2',
-        '-np', '4', '--allow-run-as-root',
-        '--display-map',
-        '--tag-output',
-        '-mca', 'btl_tcp_if_include', 'ethw3',
-        '-mca', 'oob_tcp_if_include', 'ethw3',
-        '-mca', 'plm_rsh_no_tree_spawn', '1',
-        '-bind-to', 'socket', '-map-by', 'slot',
-        '-mca', 'pml', 'ob1',
-        '-mca', 'btl', '^openib',
-        '-mca', 'orte_abort_on_non_zero_status', '1',
-        '-x', 'NCCL_MIN_NRINGS=4',
-        '-x', 'NCCL_SOCKET_IFNAME=ethw3',
-        '-x', 'NCCL_DEBUG=INFO',
-        '-x', 'LD_LIBRARY_PATH',
-        '-x', 'PATH',
-        '-x', 'LD_PRELOAD=%s' % inspect.getfile(libchangehostname),
-        '-v', '--lr', '35', '-x', 'LD_CONFIG_PATH', '/bin/sh', '-c', './train.sh -v --lr 35'],
-        cwd=_env.code_dir,
-        env=ANY, stderr=None)
+        ssh_client().load_system_host_keys.assert_called()
+        ssh_client().set_missing_host_key_policy.assert_called_with(policy())
+        ssh_client().connect.assert_called_with('algo-2', port=22)
+        ssh_client().close.assert_called()
 
-    assert process == popen()
+        popen.assert_called_with([
+            'mpirun',
+            '--host', 'algo-1:2,algo-2:2',
+            '-np', '4', '--allow-run-as-root',
+            '--display-map',
+            '--tag-output',
+            '-mca', 'btl_tcp_if_include', 'ethw3',
+            '-mca', 'oob_tcp_if_include', 'ethw3',
+            '-mca', 'plm_rsh_no_tree_spawn', '1',
+            '-bind-to', 'socket', '-map-by', 'slot',
+            '-mca', 'pml', 'ob1',
+            '-mca', 'btl', '^openib',
+            '-mca', 'orte_abort_on_non_zero_status', '1',
+            '-x', 'NCCL_MIN_NRINGS=4',
+            '-x', 'NCCL_SOCKET_IFNAME=ethw3',
+            '-x', 'NCCL_DEBUG=INFO',
+            '-x', 'LD_LIBRARY_PATH',
+            '-x', 'PATH',
+            '-x', 'LD_PRELOAD=%s' % inspect.getfile(libchangehostname),
+            '-v', '--lr', '35', '-x', 'LD_CONFIG_PATH', '/bin/sh', '-c', './train.sh -v --lr 35'],
+            cwd=_env.code_dir,
+            env=ANY, stderr=None)
+
+        assert process == popen()
